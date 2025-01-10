fed6ac2ee9119a60c07f4b75f00088c972f93351
jwahoonkim <kjhoon0330@snu.ac.kr>
2025-01-11 04:20:21 +0900

    init fixup

---
+++ src/test/kotlin/me/jahni/twolevelttlcache/TwoLevelTtlCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jahni.twolevelttlcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TwoLevelTtlCacheApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
